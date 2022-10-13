d442d7390fe0151c30a956b787b6967111cc849a
spt <Awoljack0@>
2022-10-13 14:43:28 +0600

    first commit

---
+++ src/main/java/com/example/check111/Check111Application.java
@@ -0,0 +1,13 @@
+package com.example.check111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Check111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Check111Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/check111/Check111ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.check111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Check111ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
