a92e753fc42971cf6216d5b1c27e43aad6e20be9
spt <Awoljack0@>
2022-10-21 10:37:16 +0600

    today is new day

--- src/main/java/com/example/check111/BranchOne.java
+++ src/main/java/com/example/check111/BranchOne.java
@@ -3,4 +3,6 @@ package com.example.check111;
 public class BranchOne {
     String branchOne = " branch one ";
     String branchOnet = " branch one commit 2";
+
+    String branchOnew = " today is new day";
 }
